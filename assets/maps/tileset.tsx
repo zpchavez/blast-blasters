68d178d2d45dc6f59b6ec137b4891b8136125570
Zachary Chavez <zach@rychavez.com>
2016-11-06 18:32:14 -0700

    Export tileset tsx file

---
+++ assets/maps/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" spacing="1" margin="1">
+ <image source="tileset.png" width="166" height="34"/>
+</tileset>
