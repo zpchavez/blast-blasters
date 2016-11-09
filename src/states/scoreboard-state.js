7e257d716ffe12eafd20ee2f86b7355b5bef91fd
Zachary Chavez <zach@rychavez.com>
2016-11-08 20:48:47 -0700

    Show who killed whom on the scoreboard

--- src/states/scoreboard-state.js
+++ src/states/scoreboard-state.js
@@ -6,27 +6,77 @@ import leftpad from '../util/leftpad';
 
 class ScoreboardState extends AbstractState
 {
-    constructor(roundScore)
+    constructor(playerKills)
     {
         super();
-        this.roundScore = roundScore;
+        this.playerKills = playerKills;
     }
 
     create()
     {
         super.create();
 
+        this.renderPlayerKills();
         this.renderScore();
         setTimeout(this.loadNextRound.bind(this), 3000);
     }
 
+    renderPlayerKills()
+    {
+        this.game.add.text(
+            this.game.width / 2 - 150,
+            50,
+            'Round Kills',
+            {
+                font: '40px Arial',
+                fill: '#ffffff',
+                stroke: '#ffffff',
+                strokeThickness: 2,
+            }
+        );
+        this.playerKills.forEach((kills, killer) => {
+            const color = colors[globalState.get('colors')[killer]];
+            this.game.add.text(
+                this.game.width / 2 - 200,
+                100 + ((killer + 1) * 50),
+                color.name.charAt(0).toUpperCase() + color.name.substr(1),
+                {
+                    font: '32px Arial',
+                    fill: '#' + leftpad(color.hex.toString(16), 6, 0),
+                    stroke: '#ffffff',
+                    strokeThickness: 2,
+                }
+            );
+            kills.forEach((killed, killIndex) => {
+                let playerSprite = this.game.make.sprite(
+                    this.game.width / 2 - 80 + (50 * killIndex),
+                    100 + ((killer + 1) * 50),
+                    'player'
+                );
+                playerSprite.tint = colors[globalState.get('colors')[killed]].hex;
+                this.game.world.addChild(playerSprite);
+            });
+        });
+    }
+
     renderScore()
     {
+        this.game.add.text(
+            this.game.width / 2 - 150,
+            380,
+            'Total Score',
+            {
+                font: '40px Arial',
+                fill: '#ffffff',
+                stroke: '#ffffff',
+                strokeThickness: 2,
+            }
+        );
         globalState.get('score').forEach((score, index) => {
             const color = colors[globalState.get('colors')[index]];
             this.game.add.text(
                 this.game.width / 2 - 200,
-                this.game.height / 3 + (index * 50),
+                this.game.height - 250 + (index * 50),
                 color.name.charAt(0).toUpperCase() + color.name.substr(1),
                 {
                     font: '32px Arial',
@@ -37,7 +87,7 @@ class ScoreboardState extends AbstractState
             );
             this.game.add.text(
                 this.game.width / 2 - 80,
-                this.game.height / 3 + (index * 50),
+                this.game.height - 250 + (index * 50),
                 score,
                 {
                     font: '32px Arial',
