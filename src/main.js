af544b2c76844ab2d1c4fd4c80395a977cfe9616
Zachary Chavez <zach@rychavez.com>
2016-11-20 17:23:20 -0700

    Allow skipping menus by putting player number in query string

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import MainMenuState from './states/main-menu-state';
+import GameState from './states/game-state';
+import globalState from './util/global-state';
+import queryString from 'query-string';
 
 var game = new Phaser.Game(
     1280,
@@ -8,4 +11,11 @@ var game = new Phaser.Game(
     null
 );
 
-game.state.add('game', new MainMenuState(), true);
+const queryOptions = queryString.parse(window.location.search);
+if (queryOptions.players) {
+    globalState.set('players', parseInt(queryOptions.players, 10));
+    globalState.state.round = 1;
+    game.state.add('game', new GameState(), true);
+} else {
+    game.state.add('game', new MainMenuState(), true);
+}
